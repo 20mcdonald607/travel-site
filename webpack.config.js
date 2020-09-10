8a0b7a38ac811178f083c32acaea5ac3a8aca940
Michael <michaelmemac@MikeBook-Pro-2019.local>
2020-09-10 10:39:57 -0500

    Completed desktop and mobile header styles

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const postCSSPlugins = [
   require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
+  require('postcss-hexrgba'),
   require('autoprefixer')
 ]
 
@@ -14,8 +15,16 @@ module.exports = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
   },
+  devServer: {
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
   mode: 'development',
-  watch: true,
   module: {
     rules: [
       {
